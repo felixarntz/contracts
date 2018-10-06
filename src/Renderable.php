b569dc1115cdbbb03a6ab98ce628f5036f80c18d
Felix Arntz <hello@felix-arntz.me>
2018-10-06 17:09:00 +0200

    Introduce Renderable interface.

---
+++ src/Renderable.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * Interface FelixArntz\Contracts\Renderable
+ *
+ * @package FelixArntz\Contracts
+ * @license GNU General Public License, version 2
+ * @link    https://github.com/felixarntz/contracts
+ */
+
+namespace FelixArntz\Contracts;
+
+/**
+ * Interface for a renderable.
+ *
+ * @since 1.0.0
+ */
+interface Renderable
+{
+
+    /**
+     * Renders the instance.
+     *
+     * @since 1.0.0
+     */
+    public function render();
+}
